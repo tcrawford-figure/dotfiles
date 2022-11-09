8f5f340a93a8660c2f2b8ee13e0a1d35d3a9d637
Tyler Crawford <tcrawford@figure.com>
2022-11-09 10:57:32 -0500

    Update dotfiles

--- dot_config/zshrc.d/post/autocompletion.zsh
+++ dot_config/zshrc.d/post/autocompletion.zsh
@@ -10,3 +10,23 @@ eval "$(op completion zsh)"; compdef _op op
 autoload -Uz compinit
 compinit
 
+#compdef gt
+###-begin-gt-completions-###
+#
+# yargs command completion script
+#
+# Installation: /usr/local/bin/gt completion >> ~/.zshrc
+#    or /usr/local/bin/gt completion >> ~/.zprofile on OSX.
+#
+_gt_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /usr/local/bin/gt --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _gt_yargs_completions gt
+###-end-gt-completions-###
+
